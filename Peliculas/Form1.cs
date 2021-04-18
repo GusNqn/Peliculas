cf11d66dbbb17ff58390ae7285e582e8d7d8e8d9
gustavo_seguin@hotmail.com <gustavo_seguin@hotmail.com>
2021-04-18 19:27:04 -0300

    Iniciando

--- Peliculas/Form1.cs
+++ Peliculas/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Películas
-{
+{//subiendo a Git
     public partial class fPeliculas : Form
     {
         #region variables globales
